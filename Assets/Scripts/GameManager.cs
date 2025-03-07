0412cb145f36dc40fa8accb097ec24985d313814
YusakuYokota <youcan1849@gmail.com>
2025-03-07 14:47:47 +0900

    クイズ抽選システムテスト

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int _currentQuizDifficulty = 1;
+
+    [SerializeField]
+    private QuizSelecter _selecter;
+
+    private void Update()
+    {
+        // 正解
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 出題する難易度を上げる
+            _currentQuizDifficulty++;
+            _selecter.SelectQuiz(_currentQuizDifficulty);
+        }
+        // 不正解
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // 出題する難易度を下げる
+            _currentQuizDifficulty--;
+            _selecter.SelectQuiz(_currentQuizDifficulty);
+        }
+    }
+}
---
+++ Assets/Scripts/QuizSelecter.cs
@@ -0,0 +1,38 @@
+﻿using MessagePack.Resolvers;
+using MessagePack;
+using QuestionMasterData;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.UI;
+
+public class QuizSelecter : MonoBehaviour
+{
+    private MemoryDatabase _questionDatabase;
+
+    [SerializeField]
+    private Text _questionText;
+
+    private async void Start()
+    {
+        var messagePackResolvers = CompositeResolver.Create(
+            MasterMemoryResolver.Instance,
+            GeneratedResolver.Instance,
+            StandardResolver.Instance
+        );
+        var options = MessagePackSerializerOptions.Standard.WithResolver(messagePackResolvers);
+        MessagePackSerializer.DefaultOptions = options;
+
+        var asset = Addressables.LoadAssetAsync<TextAsset>("QuestionMasterData");
+        await asset.Task;
+        var binary = asset.Result.bytes;
+
+        _questionDatabase = new MemoryDatabase(binary);
+    }
+
+    public void SelectQuiz(int difficulty)
+    {
+        var questions = _questionDatabase.QuestionMasterTable.FindRangeByDifficulty(difficulty, difficulty);
+        var randQ = Random.Range(0, questions.Count);
+        _questionText.text = questions[randQ].Kanji;
+    }
+}
